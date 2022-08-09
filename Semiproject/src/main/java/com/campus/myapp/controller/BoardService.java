d6afd05b160f4d9d29023d37b3ee62bec2caca8e
Seho0218 <ghdtpgh8913@gmail.com>
2022-08-09 23:44:12 +0900

    HSH-0809-page

---
+++ Semiproject/src/main/java/com/campus/myapp/controller/BoardService.java
@@ -0,0 +1,5 @@
+package com.campus.myapp.controller;
+
+public enum BoardService {
+
+}
---
+++ Semiproject/src/main/java/com/campus/myapp/service/BoardServiceImpl.java
@@ -0,0 +1,5 @@
+package com.campus.myapp.service;
+
+public class BoardServiceImpl implements boardService {
+    
+}
