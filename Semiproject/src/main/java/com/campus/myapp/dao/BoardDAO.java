c7d01fde484fd717d1b394d47dd80e68e3ab3641
Seho0218 <105859355+Seho0218@users.noreply.github.com>
2022-08-11 10:19:58 +0900

    HSH-0810-tin

--- Semiproject/src/main/java/com/campus/myapp/dao/BoardDAO.java
+++ Semiproject/src/main/java/com/campus/myapp/dao/BoardDAO.java
@@ -14,18 +14,18 @@ public interface BoardDAO {
 
     public int totalRecord(PagingVO pVO);
 	//글등록
-	public int boardWriteOk(BoardVO vo);
+	//public int boardWriteOk(BoardVO vo);
 	//글선택(수정). 글내용보기
-	public BoardVO getBoard(int no);
+	//public BoardVO getBoard(int no);
 	//글수정
-	public int boardEditOk(BoardVO vo);
+	//public int boardEditOk(BoardVO vo);
 	//글삭제
-	public int boardDel(int no, String userid);
+	//public int boardDel(int no, String userid);
 	//조회수 증가
-	public void hitCount(int no);
+	//public void hitCount(int no);
 	//총레코드수
-	public int totalRecord(PagingVO pVO);
+	/public int totalRecord(PagingVO pVO);
 	//여러개의 레코드 삭제
-	public int boardMultiDel(BoardVO vo);
+	//public int boardMultiDel(BoardVO vo);
 
 }
